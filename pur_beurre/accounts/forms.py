7322fec4acbfe651e7218a1bb9772c0924f9fe7a
Damien Galasso <damien.galasso@outlook.fr>
2019-05-01 12:26:03 +0200

    ' my_account' page implemented

--- pur_beurre/accounts/forms.py
+++ pur_beurre/accounts/forms.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, label="Prénom")
+    last_name = forms.CharField(max_length=30, label="Nom")
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
